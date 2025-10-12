0616206c9fdcb75950bed5536bab8210cbd226b3
JayaAnanthVenkadesh <jayaananthvenkadesh@gmail.com>
2025-10-12 23:49:03 +0100

    Oops Practise

---
+++ untitled1/src/kyword1.java
@@ -0,0 +1,19 @@
+final class Vehicle {
+
+    final int speedLimit = 120;
+
+    final void displaySpeed() {
+        System.out.println("Speed limit is: " + speedLimit + " km/h");
+    }
+}
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle v = new Vehicle();
+        v.displaySpeed();
+
+    }
+}
+
