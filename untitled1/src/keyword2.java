b47edb0dc400d83e7066dbe29a0247d08091e47f
JayaAnanthVenkadesh <jayaananthvenkadesh@gmail.com>
2025-10-12 23:53:01 +0100

    Oops Practise

---
+++ untitled1/src/keyword2.java
@@ -0,0 +1,42 @@
+class Animal {
+    String name = "Animal";
+
+
+    Animal() {
+        System.out.println("Animal constructor called");
+    }
+
+    // Parent class method
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+
+class Dog extends Animal {
+    String name = "Dog";
+
+    Dog() {
+        super();
+        System.out.println("Dog constructor called");
+    }
+
+    void displayNames() {
+        System.out.println("Parent name: " + super.name);
+        System.out.println("Child name: " + name);
+    }
+
+    void sound() {
+        // Call parent class method
+        super.sound();
+        System.out.println("Dog barks");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.displayNames();
+        d.sound();
+    }
+}
